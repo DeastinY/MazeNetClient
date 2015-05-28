b52bc429e0f5675a5f6b123f5083c1df9fde0b72
dnikolay <daniel.nikolay@rwth-aachen.de>
2015-05-28 13:07:52 +0200

    classes lost ...

---
+++ MazeNetClient/AI/IRatingStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeNetClient.Game;
+
+namespace MazeNetClient.AI
+{
+    interface IRatingStrategy
+    {
+        Tuple<Move, float> GetBestMove(SimulatedBoard board, List<Field> moves);
+    }
+}
---
+++ MazeNetClient/AI/StupidRating.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeNetClient.Game;
+
+namespace MazeNetClient.AI
+{
+    class StupidRating :IRatingStrategy
+    {
+        private SimulatedBoard board;
+        public Tuple<Move, float> GetBestMove(SimulatedBoard board, List<Field> moves)
+        {
+            this.board = board;
+            Field bestField = null;
+            float bestRating = 0f;
+
+            foreach(Field f in moves)
+            {
+                float rating = this.RateMove(f);
+
+                if(rating>bestRating)
+                {
+                    bestField = f;
+                    bestRating = rating;
+                }
+            }
+
+            Move move = new Move(bestField.RowIndex, bestField.ColumnIndex,
+                board.ShiftPositionRowIndex, board.ShiftPositionColumnIndex, board.ShiftCardRotation);
+            return Tuple.Create(move, bestRating);
+        }
+
+        private float RateMove(Field move)
+        {
+            Random r = new Random();
+            return (float) r.NextDouble();
+        }
+    }
+}
