041b2edbf66e11421a0077c36c630131460c199b
dnikolay <daniel.nikolay@rwth-aachen.de>
2015-05-28 10:10:15 +0200

    added Evaluator and First Strategy

---
+++ MazeNetClient/AI/Evaluator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeNetClient.AI
+{
+    static class Evaluator
+    {
+        internal static Move GetBestMove(List<SimulatedBoard> container, IEvaluationStrategy strategy)
+        {
+            var moves = new List<Tuple<Move, float>>(container.Count);
+
+            foreach (SimulatedBoard sb in container)
+            {
+                Tuple<Move, float> temp = strategy.GetBestMove(sb);
+                moves.Add(temp);
+            }
+
+            Tuple<Move, float> bestMove = moves[0];
+
+            for (int i = 1; i < moves.Count; ++i)
+            {
+                if (moves[i].Item2 > bestMove.Item2)
+                    bestMove = moves[i];
+            }
+
+            return bestMove.Item1;
+        }
+    }
+}
---
+++ MazeNetClient/AI/StupidEvaluation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeNetClient.AI
+{
+    class StupidEvaluation :IEvaluationStrategy
+    {
+        public Tuple<Move, float> GetBestMove(SimulatedBoard board)
+        {
+            Move move = new Move(board.PlayerPositionRowIndex, board.PlayerPositionColumnIndex,
+                board.ShiftPositionRowIndex, board.ShiftPositionColumnIndex, board.ShiftCardRotation);
+            float result = 0.5f;
+
+            return Tuple.Create(move, result);
+        }
+    }
+}
