5734e88270fdcf8f7a48a6f3200b70dc5d78e2ab
Richard Polzin <polzin.richard@gmail.com>
2015-06-10 19:01:25 +0200

    Got some errors with the post

--- MazeNetEvaluator/evaluator.py
+++ MazeNetEvaluator/evaluator.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from werkzeug import secure_filename
 
 UPLOAD_FOLDER = '/uploads/'
@@ -8,19 +8,29 @@ ALLOWED_EXTENSIONS = set('exe')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
+
 @app.route('/home')
 def home():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST', 'GET'])
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
+    print(request.get_data())
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
@@ -29,4 +39,4 @@ def upload_file():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
