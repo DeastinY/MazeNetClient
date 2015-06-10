2f2a8f4f642c698a332255d84b5eb3fa43102687
Richard Polzin <polzin.richard@gmail.com>
2015-06-10 18:34:31 +0200

    I hate fucking html css bullshit and never want to deal with it again.
    <^))><

--- MazeNetEvaluator/evaluator.py
+++ MazeNetEvaluator/evaluator.py
@@ -1,12 +1,32 @@
+import os
 from flask import Flask, render_template
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/uploads/'
+ALLOWED_EXTENSIONS = set('exe')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def main_menu():
+def main():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload_file():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config[UPLOAD_FOLDER], filename))
+        return redirect(url_for('uploaded_file, filename=filename'))
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
